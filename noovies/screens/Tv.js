69ebe1c989003628954dba29f3867ba4346ecfd3
doyeon <60147434+doyeon111@users.noreply.github.com>
2021-11-25 10:08:27 +0900

    TV 페이지 설정

--- noovies/screens/Tv.js
+++ noovies/screens/Tv.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { tvApi } from "../api";
 
 export default () => {
-        const [shows, setShows] = useState({
+        const [shows, setShows] = useState({ //tv api의 데이터 불러옴
             today: [],
             thisWeek: [],
             topRated: [],
@@ -14,10 +14,10 @@ export default () => {
             popularError: null
         });
     const getData = async() => {
-        const [today, todayError] = await tvApi.today();
-        const [thisWeek, thisWeekError] = await tvApi.thisWeek();
-        const [topRated, topRatedError] = await tvApi.topRated();
-        const [popular, popularError] = await tvApi.popular();
+        const [today, todayError] = await tvApi.today(); // 오늘의 인기 tv
+        const [thisWeek, thisWeekError] = await tvApi.thisWeek(); //이번 주의 인기있는 tv
+        const [topRated, topRatedError] = await tvApi.topRated(); //가장 인기있는 tv 
+        const [popular, popularError] = await tvApi.popular(); //요즘 인기있는 tv
         setShows({
             today,
             thisWeek,
