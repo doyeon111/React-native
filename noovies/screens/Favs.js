6955fd80a3feed885641efccc2e00f3ab0d60f06
doyeon <60147434+doyeon111@users.noreply.github.com>
2021-11-25 10:06:50 +0900

    좋아하는 영화 설정 페이지

--- noovies/screens/Favs.js
+++ noovies/screens/Favs.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from 'react-native';
-import { movieApi } from "../api";
+import { movieApi } from "../api"; //api 모듈 불러옴
 
 export default () => {
     const [movies, setMovies] = useState({
