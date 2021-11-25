9b2383c928e9fce136e5db51a225e9c3e4209b3d
doyeon <60147434+doyeon111@users.noreply.github.com>
2021-11-25 09:57:42 +0900

    api 페이지

--- noovies/api.js
+++ noovies/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TMDB_KEY = "928b9262b283510a11bec725278283e4"
+const TMDB_KEY = "928b9262b283510a11bec725278283e4" //영화 사이트에서 나의 개인 api 키를 가져옴.
 
 const makeRequest = (path, params) => axios.get(`https://api.themoviedb.org/3${path}`, {
     params: {
@@ -23,7 +23,7 @@ const getAnything = async(path, params = {}) => {
 
 
 
-export const movieApi = {
+export const movieApi = { // 영화 api를 아이디로 가져온다.
     nowPlaying: () => getAnything("/movie/now_playing"), 
     popular: () => getAnything("/movie/popular"),
     upcoming: () => getAnything("/movie/upcoming", {"region": "kr"}),
@@ -32,7 +32,7 @@ export const movieApi = {
     discover: () => getAnything("/discover/movie")
 };
 
-export const tvApi = {
+export const tvApi = { // tv api를 가져오는 방법
     today: () => getAnything("/tv/airing_today"),
     thisWeek: () => getAnything("/tv/on_the_air"),
     topRated: () => getAnything("/tv/top_rated"),
